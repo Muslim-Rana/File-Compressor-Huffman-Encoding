acc2b38608752dd05c92fcd5c1d42c835d77a87f
Muslim-Rana <muslim.inaam.rana@gmail.com>
2023-07-27 18:22:50 -0400

    Updated Encode.java, added a sample large .txt file
    - In the main function of Encode.java, we read every byte (character) of our sample .txt file and keep track of its frequency.

--- src/Encode.java
+++ src/Encode.java
@@ -1,2 +1,54 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Hashtable;
+
 public class Encode {
+    public static void main(String[] args) {
+        //Initial variables
+        FileInputStream in;
+        Hashtable<Character, Integer> ht = new Hashtable<Character, Integer>();
+
+
+        try {
+
+            int c;
+            in = new FileInputStream("warandpeace.txt");
+
+            System.out.println("Began reading the file");
+
+            //Storing the frequencies of each character in a hashtable alongside the character itself
+            while (true) {
+                try {
+                    c = in.read();
+                    if (c < 0) break;
+                    if (ht.get((char) c) == null) {
+                        ht.put((char) c, 1);
+                    }
+                    else {
+                        ht.replace((char) c, ht.get((char) c) + 1);
+                    }
+
+
+                }
+                catch (IOException e) {
+                    System.err.println("I/O ERROR OCCURRED");
+
+                }
+                catch (NullPointerException e) {
+                    System.err.println("NULL POINTER ERROR OCCURRED");
+                }
+            }
+            in.close();
+            System.out.println("Finished reading the file");
+
+
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("FILE NOT FOUND" + e.toString());
+        }
+        catch (IOException e) {
+            System.err.println("I/O ERROR OCCURRED" + e.toString());
+        }
+    }
 }
