7cf80aee18ab380659f001e10c1a1022de324079
Muslim-Rana <muslim.inaam.rana@gmail.com>
2023-08-01 20:24:51 -0400

    Updated Encode.java
    - Completed the implementation of the sortArray method.

--- src/Encode.java
+++ src/Encode.java
@@ -15,8 +15,21 @@ public class Encode {
         //Traversing the Node array from the start until the final index
         for (int i = 0; i < n.size(); i++) {
 
-            for (int j = 0; j < n.size() - 1 - i; j++) {
+            for (int j = 0; j< n.size() - 1 - i; j++) {
 
+                //Comparing the Node at index j with the following Node
+                //Swapping them if the Node at j+1 has a smaller frequency
+                if (n.get(j).freq > n.get(j+1).freq) {
+                    char tempV = n.get(j+1).value;
+                    int tempF = n.get(j+1).freq;
+
+                    n.get(j+1).value = n.get(j).value;
+                    n.get(j+1).freq = n.get(j).freq;
+
+                    n.get(j).value = tempV;
+                    n.get(j).freq = tempF;
+
+                }
             }
         }
 
@@ -73,7 +86,8 @@ public class Encode {
         ht.forEach((k,v) -> {
             nodes_array.add(new Node (k,v));
         });
+        sortArray(nodes_array);
 
-        System.out.println("Nodes stored in arraylist");
+        System.out.println("Nodes stored in arraylist and sorted");
     }
 }
