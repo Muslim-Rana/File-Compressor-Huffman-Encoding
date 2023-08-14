16ec16abfa658e8c0eefb08cf6694abd560cb3f1
Muslim-Rana <muslim.inaam.rana@gmail.com>
2023-08-14 17:42:11 -0400

    Created buildStringSet and writeEncode methods in Encode.java
    - buildStringSet is a method which takes a huffman tree and finds a binary representation for each character based on that huffman tree. This is where the optimization happens, the most frequent characters get smaller binary representations.
    
    - writeEncode takes our new binary representations for each character and writes them out to file.encoded one by one.

--- src/Encode.java
+++ src/Encode.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.Hashtable;
 
 public class Encode {
@@ -69,10 +70,96 @@ public class Encode {
         return nodes_array.get(0);
     }
 
+    /**
+     * A method for storing the binary representations of different characters based on a binary tree sorted by frequencies.
+     * @param n The root Node of your binary tree
+     * @param bits An initial empty string to which 0s and 1s will be added
+     * @param ht The hashtable where each character will be stored alongside its binary representation (the binary representation will be a String)
+     */
+    public static void buildStringSet (Node n, String bits, Hashtable<Character, String> ht) {
+        //Storing the value once we have reached a leaf node.
+        if (n.leftNode == null && n.rightNode == null) {
+            ht.put(n.value, bits);
+        }
+
+        if (n.leftNode != null) {
+            buildStringSet(n.leftNode, bits + "0", ht);
+        }
+        if (n.rightNode != null) {
+            buildStringSet(n.rightNode, bits + "1", ht);
+        }
+    }
+
+    /**
+     * This method writes our encoded file. It needs us to name the file we want to read and then encode
+     * and it needs us to provide it with a hashtable of the binary representation of each character
+     *
+     * @param fileName The file we want to encode
+     * @param stringSetTable The characters in that file and their binary representations
+     */
+    public static void writeEncode(String fileName,Hashtable <Character, String> stringSetTable ) {
+        try {
+            int c;
+            BitSet bSet = new BitSet();
+            int index = 0;
+            FileInputStream in = new FileInputStream(fileName);
+
+
+            while (true) {
+                c = in.read();
+                if (c<0) break;
+
+                //Getting the binary representation of the current character we are reading
+                String bitString = stringSetTable.get((char) c);
+
+
+                if (bitString == null) {
+                    System.out.println(c);
+                    throw new Exception("Invalid Character Found!");
+                }
+
+                //Transitioning from a bitString (binary representation of a character as a string of 0s and 1s) to a BitSet
+                //We either set the BitSet to true or false based on whether we have a 1 or 0 in our string
+                /*The for loop runs from 0 to the end of the bitString. When the while true loop runs again our index would have
+                 * already been incremented this means that we will start setting 0s and 1s from where we left off
+                 */
+                for (int i = 0; i < bitString.length(); i++) {
+                    if (bitString.charAt(i) == '1') {
+                        bSet.set(index);
+                    }
+                    else if (bitString.charAt(i) == '0') {
+                        bSet.set(index, false);
+                    }
+                    index++;
+                }
+            }
+            in.close();
+
+            //Writing the encoded file as a ByteArray
+            /*We are also writing the index out, the reasoning for this is that neither BitSet.length() nor BitSet.size() were
+             * giving us the correct size (equal to the index). This caused trouble during the decoding portion. In order to counter this we will just send
+             * over the index as well and use it in our for loop conditions instead of BitSet.size() and BitSet.length()
+             *
+             */
+            FileOutputStream fileOut = new FileOutputStream("File.encoded");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(index);
+            out.writeObject(bSet.toByteArray());
+            out.close();
+            fileOut.close();
+        }
+
+        catch (Exception e){
+            System.err.println (e.toString());
+
+        }
+    }
+
     public static void main(String[] args) {
         //Initial variables
         FileInputStream in;
         Hashtable<Character, Integer> ht = new Hashtable<Character, Integer>();
+        Hashtable <Character, String> stringSetTable = new Hashtable<Character,String>();
         ArrayList<Node> nodes_array = new ArrayList<Node>();
 
 
@@ -128,6 +215,12 @@ public class Encode {
             fileOut.close();
 
             System.out.println("Tree Serialized");
+
+            buildStringSet(tree, "", stringSetTable);
+            writeEncode("warandpeace.txt", stringSetTable);
+            System.out.println("Encoding complete!");
+
+
 
 
         }
