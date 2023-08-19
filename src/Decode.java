a604d256ce8deb59bba486fd8622500840c821e4
Muslim-Rana <muslim.inaam.rana@gmail.com>
2023-08-18 20:28:41 -0400

    Updated Decode.java
    - Created a method deserializeTree which takes in the Tree.ser file produced from Encode.java, parses it, and returns the root node of the Huffman Tree.

--- src/Decode.java
+++ src/Decode.java
@@ -1,2 +1,40 @@
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 public class Decode {
+
+    /**
+     * Deserializes the tree and gives us the root node of our binary tree
+     * @param filename The name of the file where the tree has been stored
+     * @return The root node of the tree
+     * @throws Exception Catches any general exception found using catch Exception e
+     */
+    public static Node deserializeTree (String filename) throws Exception {
+        Node tree = null;
+        FileInputStream fileIn = new FileInputStream(filename);
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        tree = (Node) in.readObject();
+        in.close();
+        fileIn.close();
+        return tree;
+    }
+
+
+
+    public static void main (String [] args) {
+
+        try {
+            System.out.println("Deserializing the Huffman Tree...");
+            Node tree = deserializeTree("Tree.ser");
+            System.out.println("Tree deserialized");
+        }
+
+
+        catch (Exception e) {
+            // Error code goes here when an I/O exception occurs
+            System.err.println (e.toString());
+        }
+
+
+    }
 }
