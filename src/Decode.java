1a25e2c225631d0b06d814cbf714286ce81130df
Muslim-Rana <muslim.inaam.rana@gmail.com>
2023-08-23 13:49:23 -0400

    - Updated Decode.java
    - Implemented two new methods decodeFile and writeDecodedText. These two methods work together to produce our output file File.decoded which is exactly the same as the original text file given to our program during the encoding stage.

--- src/Decode.java
+++ src/Decode.java
@@ -1,5 +1,7 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
+import java.util.BitSet;
 
 public class Decode {
 
@@ -19,14 +21,78 @@ public class Decode {
         return tree;
     }
 
+    /*
+     * Takes the encoded file's name and the root node of our tree. It will deserialize the index and the ByteArray and then
+     * convert the ByteArray to a BitSet as well. It will then pass the BitSet, Index, tree, and a file name (for our output file)
+     * to decodeFile which will do the meat of the work
+     */
+    public static void writeDecodedText (String filename, Node tree) throws Exception {
+        byte[] bArray = null;
+        FileInputStream fileIn = new FileInputStream(filename);
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        int index = (int) in.readObject();
+        bArray = (byte[]) in.readObject();
+        in.close();
+        fileIn.close();
 
+        BitSet b = BitSet.valueOf(bArray);
+        decodeFile(b,index,tree,"File.decoded");
+
+    }
+
+    /**
+     * Creates File.decoded which should be identical to our original text piece which we encoded
+     *
+     * @param b The BitSet created from the encoded ByteArray in writeDecodedText
+     * @param index The index (length) of the BitSet
+     * @param tree The Huffman tree
+     * @param filename The name of the file we want to create
+     */
+    public static void decodeFile (BitSet b, int index, Node tree, String filename) {
+        try {
+            Node currentNode = tree;
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            //ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+
+            for (int i = 0; i < index; i++) {
+
+                if (b.get(i) == false) {
+                    currentNode = currentNode.leftNode;
+                }
+
+                else {
+                    currentNode = currentNode.rightNode;
+                }
+
+                //Writing out the character to the file once we reach a leaf node
+                if (currentNode.leftNode == null && currentNode.rightNode == null) {
+                    fileOut.write((byte)currentNode.value);
+
+                    //Resetting the current node to the root node
+                    currentNode = tree;
+                }
+            }
+            //out.close();
+            fileOut.close();
+        }
+        catch (Exception e){
+            System.err.println (e.toString());
+
+        }
+    }
 
     public static void main (String [] args) {
+
 
         try {
             System.out.println("Deserializing the Huffman Tree...");
             Node tree = deserializeTree("Tree.ser");
-            System.out.println("Tree deserialized");
+
+            System.out.println("Writing decoded file...");
+            writeDecodedText("File.encoded",tree);
+
+            System.out.println("Decoding complete!");
         }
 
 
